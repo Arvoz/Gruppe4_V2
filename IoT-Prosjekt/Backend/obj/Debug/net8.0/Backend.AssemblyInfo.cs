e3b876a1811dcb12092b1f2c10a9ff76f64f57c2
Erling <erlingkris97@hotmail.com>
2024-11-04 22:55:02 +0100

    Test  om alt funker

--- IoT-Prosjekt/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ IoT-Prosjekt/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a65c3e586593d48131ecad73e3d8c48184e56bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
